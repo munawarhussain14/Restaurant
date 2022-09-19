36aea88c4a526501b2320e0db14e76f3371cac93
Munawar Hussain <munawarhussain10@gmail.com>
2022-09-19 22:34:41 +0500

    URL Updates

--- system/config/app.php
+++ system/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Give Away Tips'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://giveaway.tips'),
 
     'asset_url' => env('ASSET_URL', null),
 
